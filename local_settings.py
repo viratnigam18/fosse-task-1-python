a17f6caf1b8c07e430549afdde8ac18f5b028321
VIRAT NIGAM <64016810+viratnigam18@users.noreply.github.com>
2025-09-14 15:55:40 +0530

    progress 1
    html , css and JavaScript basic

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+#This file contains all information related to admin credentials for email,
+# db etc.
+
+
+EMAIL_HOST = 'YOUR HOST'
+EMAIL_PORT = 'YOUR HOST PORT'
+EMAIL_HOST_USER = 'Your mail'
+EMAIL_HOST_PASSWORD = 'Your Password'
+EMAIL_USE_TLS = True
+SENDER_EMAIL = 'YOUR PRODUCTION MAIL'
